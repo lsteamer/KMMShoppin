03c7ef5c5443c6bee1bf07904a4b217fdb2f7266
Antonio Gonzalez <anjegonz@gmail.com>
2025-06-03 11:55:19 +0200

    init

---
+++ composeApp/src/commonMain/kotlin/com/anjegonz/kmmshoppin/App.kt
@@ -0,0 +1,26 @@
+package com.anjegonz.kmmshoppin
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import kmmshoppin.composeapp.generated.resources.Res
+import kmmshoppin.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+    }
+}
